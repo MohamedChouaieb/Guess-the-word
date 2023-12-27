112ad22d847209663f999c5354a37868d8a93dfb
unknown <mousscha606@gmail.com>
2023-12-27 14:10:54 +0100

    Almost Done

--- main.js
+++ main.js
@@ -15,26 +15,50 @@ function display(){
     button.classList.add("button-container");
     main.append(button);
 }
+window.onload=display()
 //Random number
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-//Random variable to conserve the generated word
+//Variable to conserve the generated word
 let random;
 //Random word selection
 function generateWord(object){
     let len=(Object.values(category[object])).length
-    var array=Object.values(category[object])
+    let array=Object.values(category[object])
     random=(array[getRandomInt(len)])
+    console.log(random)
+    let buttons = document.getElementsByTagName('category');
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+    }
+    hints(random)
 }
-//to check the user choice and the generated word
+//Hints about the random word
+var hint=document.createElement("p")
+function hints(random){
+    hint.innerHTML="<h3>● The word start with the letter : "+random[0]+"</h3><h3>● The word's length ="+random.length+"</h3>"
+    game.append(hint)
+}
+//Counter of trials
+var tryCount=0
+function start(){
+    if ((tryCount<6) && (check())) {
+        win()
+        return 
+    }
+    if(tryCount===6){
+        lost()
+    }
+    else{
+        tryAgin(tryCount)
+        tryCount++
+    }
+}
+//Check the user choice and the generated word
 function check(){
     var choice=document.getElementById("choice").value
-    if (choice.toUpperCase()===random.toUpperCase()) {
-        alert("Congratulation !")
-        return
-        }
-    alert("Try again !")   
+    return(choice.toUpperCase()===random.toUpperCase())
 }
 //Switch between hide and visible
 function toggle(id){
@@ -45,4 +69,26 @@ function toggle(id){
         any.style.display = "none";
     }
 }
-window.onload=display()
+//In case of wrong answer
+var alert_warning=document.getElementById("alert_warning")
+var ttry=document.createElement("p")
+function tryAgin(i){
+    if (i<5) {
+        ttry.innerHTML=" You still got "+(5-i)+" trial remaining !"
+    } 
+    else {
+        ttry.innerHTML=" Your last chance !"
+    }
+    alert_warning.append(ttry)
+    if(!(toggle("alert_warning"))){
+        toggle("alert_warning")   
+    }
+}
+//In case of winnig 
+function win(){
+    toggle("alert_success")
+}
+//In case of losing the game or withdrawing
+function lost(){
+    toggle("alert")
+}
